29a2ade237f5cef534ac3a6379490b248838e8db
anthony <diomede1991@libero.it>
2022-04-26 15:25:10 +0200

    manca l'array e il bonus

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    $saluto = [
+        'saluto' => 'ciao',
+        'per' => 'tutti'
+    ]
     return view('welcome');
 });
